e8c2c1cf8bd5a717e1702252f89f12962b04dadf
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2023-12-07 14:32:33 +0300

    basic styled components

---
+++ src/app/components/Basic/Container.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+import {
+  color,
+  compose,
+  layout,
+  space,
+  typography,
+  variant,
+  border,
+  zIndex,
+  position,
+} from 'styled-system';
+import { BoxProps } from './types';
+
+export const Container = styled.View<BoxProps>`
+  ${compose(color, layout, space, typography, variant, border, zIndex, position)};
+`;
---
+++ src/app/components/Basic/types.ts
@@ -0,0 +1,215 @@
+import { ReactNode } from 'react';
+import {
+  ColorProps,
+  LayoutProps,
+  TypographyProps,
+  SpaceProps,
+  FlexProps,
+  JustifyContentProps,
+  AlignItemsProps,
+  FlexDirectionProps,
+  FlexGrowProps,
+  FontSizeProps,
+  FontStyleProps,
+  FontWeightProps,
+  AlignSelfProps,
+  JustifySelfProps,
+  BoxShadowProps,
+  BorderProps,
+  FlexboxProps,
+  FlexBasisProps,
+  GridGapProps,
+  FontFamilyProps,
+  PositionProps,
+  MarginProps,
+  BackgroundImageProps,
+  ZIndexProps,
+  SizeProps,
+  WidthProps,
+  BackgroundPositionProps,
+  BackgroundProps,
+  BackgroundSizeProps,
+  BorderRadiusProps,
+  OpacityProps,
+  HeightProps,
+  ShadowProps,
+} from 'styled-system';
+
+export interface ButtonProps
+  extends ColorProps,
+    LayoutProps,
+    JustifyContentProps,
+    TypographyProps,
+    SpaceProps,
+    PositionProps,
+    BorderProps,
+    FlexDirectionProps,
+    AlignSelfProps,
+    JustifySelfProps,
+    AlignItemsProps,
+    ZIndexProps {
+  variant?:
+    | 'primary'
+    | 'secondary'
+    | 'tertiary'
+    | 'outlined'
+    | 'status'
+    | 'padded'
+    | 'paddedOutline'
+    | 'ghost'
+    | 'normal'
+    | 'rightOutlinedLeaf';
+  loading?: boolean;
+  children?: ReactNode;
+  onPress?: () => void;
+  type?: string;
+  disabled?: boolean;
+}
+
+interface Style {
+  [key: string]: string | number;
+}
+
+export interface BoxProps
+  extends SpaceProps,
+    ColorProps,
+    PositionProps,
+    GridGapProps,
+    LayoutProps,
+    FlexBasisProps,
+    FlexGrowProps,
+    FlexProps,
+    JustifyContentProps,
+    AlignItemsProps,
+    FlexDirectionProps,
+    FlexboxProps,
+    TypographyProps,
+    BorderProps,
+    BackgroundImageProps,
+    BackgroundPositionProps,
+    BoxShadowProps,
+    BackgroundSizeProps,
+    OpacityProps,
+    ShadowProps,
+    BackgroundProps {
+  transition?: string | Array<string>;
+  transform?: string | Array<string>;
+  transformOrigin?: string | Array<string>;
+  cursor?: string | Array<string>;
+  children?: ReactNode;
+  Style?: Style;
+}
+
+export interface TextInputProps
+  extends ColorProps,
+    TypographyProps,
+    FontSizeProps,
+    FontStyleProps,
+    FontWeightProps,
+    SpaceProps,
+    LayoutProps,
+    BorderProps,
+    AlignSelfProps,
+    JustifySelfProps,
+    PositionProps,
+    MarginProps,
+    FontFamilyProps {}
+
+export interface TextProps
+  extends ColorProps,
+    TypographyProps,
+    FontSizeProps,
+    FontStyleProps,
+    FontWeightProps,
+    SpaceProps,
+    LayoutProps,
+    BorderProps,
+    AlignSelfProps,
+    JustifySelfProps,
+    PositionProps,
+    MarginProps,
+    FontFamilyProps {
+  variant?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'base' | 'ellipsis';
+  textOverflow?: string;
+  cursor?: string;
+  children: string | JSX.Element[];
+}
+
+export interface InputProps
+  extends ColorProps,
+    LayoutProps,
+    TypographyProps,
+    SpaceProps,
+    AlignSelfProps,
+    JustifySelfProps,
+    BorderProps,
+    PositionProps,
+    BorderRadiusProps {
+  variant?: 'outlined' | 'filled';
+  valid?: boolean;
+  tabIndex?: number;
+}
+export interface StyledInputProps extends InputProps {
+  label: string;
+  name: string;
+  placeholder?: string;
+  type?: string;
+}
+
+export interface ListProps
+  extends ColorProps,
+    LayoutProps,
+    SpaceProps,
+    BorderProps,
+    PositionProps,
+    FlexProps,
+    FlexGrowProps,
+    JustifyContentProps,
+    AlignItemsProps {
+  type?: 'horizontal' | 'vertical' | 'spread' | string[];
+  transition?: string;
+  transform?: string;
+}
+
+export interface ImageProps
+  extends SpaceProps,
+    LayoutProps,
+    JustifySelfProps,
+    AlignSelfProps,
+    ZIndexProps,
+    PositionProps,
+    SizeProps,
+    WidthProps,
+    HeightProps,
+    BorderProps {
+  source: string;
+}
+
+export interface CardProps
+  extends BoxShadowProps,
+    AlignSelfProps,
+    JustifySelfProps,
+    JustifyContentProps,
+    SpaceProps,
+    LayoutProps,
+    BorderProps,
+    ColorProps,
+    AlignItemsProps {
+  variant?: 'default' | 'rightBended' | 'dense' | 'medium' | 'radiant';
+}
+
+export interface ImageBackgroundProps
+  extends SpaceProps,
+    LayoutProps,
+    JustifySelfProps,
+    AlignSelfProps,
+    ZIndexProps,
+    PositionProps,
+    SizeProps,
+    WidthProps,
+    HeightProps,
+    BorderProps {
+  source: string;
+  resizeMode: string;
+  children?: JSX.Element | JSX.Element[];
+}
